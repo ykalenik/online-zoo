31ab2c8718b38fb29f9ce2ef4183376ccc6f8d42
Yana <onigiri2391@outlook.com>
2022-12-11 18:40:02 +0100

    feat: implement slider in Testimonials section on Main page

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,6 +1,8 @@
 const smallScreenMql = window.matchMedia('(max-width: 640px)');
 const testimonialTexts = document.querySelectorAll('.testimonial-text');
+const testimonials = document.querySelectorAll('.testimonial');
 const body = document.querySelector('body');
+const testimonialsSlider = document.querySelector('#testimonials-slider');
 
 
 testimonialTexts.forEach((el) => el.addEventListener('click', (e) => {
@@ -22,3 +24,37 @@ testimonialTexts.forEach((el) => el.addEventListener('click', (e) => {
         body.appendChild(popupEl);
       }
 }));
+
+
+let previousSliderValue = new Number(testimonialsSlider.value) - 1;
+testimonialsSlider.addEventListener('change', (e) => {
+    const max = new Number(testimonialsSlider.getAttribute('max')) - 1;
+    const step = new Number(testimonialsSlider.getAttribute('step'));
+    const curr = new Number(testimonialsSlider.value) - 1;
+
+    const indexesToShow = [];
+    if (curr > previousSliderValue) {
+        if (curr - previousSliderValue > step) {
+            for (let i = curr - step + 1; i <= curr; i++) {
+                indexesToShow.push(i);
+            }
+        } else {
+            for (let i = previousSliderValue + 1; i <= previousSliderValue + step; i++) {
+                indexesToShow.push(i);
+            }
+        }
+    } else {
+        for (let i = curr - step + 1; i <= curr; i++) {
+            indexesToShow.push(i);
+        }
+    }
+
+    for(let i = 0; i <= max; i++) {
+        if (indexesToShow.includes(i)) {
+            testimonials[i].classList.remove('hidden');
+        } else {
+            testimonials[i].classList.add('hidden');
+        }
+    }
+    previousSliderValue = new Number(e.target.value) - 1;
+});
