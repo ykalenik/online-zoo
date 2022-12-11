8618729f388f515b6a0da57516a7a58e80e8134f
Yana <onigiri2391@outlook.com>
2022-12-11 15:17:26 +0100

    feat: automatically highlight selected amount in Peek a Friend section on Donation page

--- pages/donate/script.js
+++ pages/donate/script.js
@@ -1,6 +1,46 @@
-const paymentAmountInput = document.querySelector('.payment-form input[name="another-amount"]');
+const customPaymentAmountInput = document.querySelector('.payment-form input[name="another-amount"]');
+const paymentAmountInputs = document.querySelectorAll('input[name=payment-amout]');
+const paymentAmountWrappers = document.querySelectorAll('.payment-slider-wrapper');
 
-paymentAmountInput.oninput = (event) => {
-    if (paymentAmountInput.value.length > paymentAmountInput.maxLength) {
-        paymentAmountInput.value = paymentAmountInput.value.slice(0, paymentAmountInput.maxLength);
-}};
+window.addEventListener("resize", (e) => {
+    const width = window.innerWidth;
+    if (width <= 1000 && width >= 640) {
+        hideAmouts(['5000']);
+        showAmouts(['2000', '1000']);
+    } else if (width < 640) {
+        hideAmouts(['5000', '2000', '1000']);
+    } else {
+        paymentAmountWrappers.forEach((el) => el.classList.remove('hidden'));
+    }
+});
+
+paymentAmountInputs.forEach((el) => el.addEventListener('click', (e) => {
+    customPaymentAmountInput.value = e.target.value;
+}));
+
+customPaymentAmountInput.oninput = (event) => {
+    if (customPaymentAmountInput.value.length > customPaymentAmountInput.maxLength) {
+        customPaymentAmountInput.value = customPaymentAmountInput.value.slice(0, customPaymentAmountInput.maxLength);
+    }
+    paymentAmountInputs.forEach((amountEl) => {
+        amountEl.checked = event.target.value === amountEl.value;
+    })
+};
+
+function hideAmouts(amountsToHide) {
+    paymentAmountWrappers.forEach((el) => {
+        const currentPaymentAmout = el.querySelector('input').value;
+        if (amountsToHide.includes(currentPaymentAmout)) {
+            el.classList.add('hidden');
+        }
+    });
+}
+
+function showAmouts(amountsToShow) {
+    paymentAmountWrappers.forEach((el) => {
+        const currentPaymentAmout = el.querySelector('input').value;
+        if (amountsToShow.includes(currentPaymentAmout)) {
+            el.classList.remove('hidden');
+        }
+    });
+}
